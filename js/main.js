25dd40641e065cbd74b518be27e86cde4115c690
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-01 15:26:35 +0300

    create: main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-var mySwiper = new Swiper('.slider-container', {
-    // Optional parameters
-    loop: true,
-    // direction: 'vertical',
-    autoplay: {
-        delay: 3000,
-      },
-      
-
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
-  })
+window.addEventListener('DOMContentLoaded', () => {
+    
+})
