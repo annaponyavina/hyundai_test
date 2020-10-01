33381d0a12b38ca2dac99e9924b193b6a842f8ac
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-01 22:13:14 +0300

    create: validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    const sendForm = () => {
+        const requestForm = document.querySelector('.request-form');
+
+const emailReg = /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/;
+
+
+
+        requestForm.addEventListener('input', (event) => {
+            let target = event.target;
+
+            if (target.getAttribute('type') === 'text') {
+                target.value = target.value.replace(/[^a-z]/g, '');
+            } 
+            if (target.getAttribute('type') === 'tel') {
+                target.value = target.value.replace(/[^\d]/g, '');
+            }
+            if (target.getAttribute('type') === 'email') {
+                if(!emailReg.test(target.value)) {
+                    console.log('опачки');
+                }
+            }
+
+            
+
+        });
+
+
+    };
+    sendForm();
+
 });
